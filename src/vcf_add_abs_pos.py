b497818735d521e47d074d0e2302da1e8133e58c
Attila Gulyas-Kovacs <attila.gulyas.kovacs@gmail.com>
2018-10-05 15:32:21 -0400

    Output VCF with offsetpositions

--- src/vcf_add_abs_pos.py
+++ src/vcf_add_abs_pos.py
@@ -6,12 +6,13 @@ import vcf
 import csv
 
 invcfname = sys.argv[1]
-if len(sys.argv) <= 2:
-    faifname = os.environ["REFSEQ"] + ".fai"
-else:
-    faifname = sys.argv[2]
+#outvcfname = "offsetpos-" + invcfname
+faifname = os.environ["REFSEQ"] + ".fai"
 
 def fai2dict(fai_file):
+    """Read a fasta index from path fai_file and create a dictionary with
+    contig names as keys and offset as values.
+    """
     with open(fai_file, "r") as f:
         csv_reader = csv.reader(f, delimiter = "\t")
         d = {}
@@ -19,13 +20,20 @@ def fai2dict(fai_file):
             d[r[0]] = int(r[2])
         return(d)
 
-def print_records(invcf_file, fai, nrec = 2):
+def print_records(invcf_file, outvcf_file = invcfname + ".offsetpos", faidict = fai2dict(faifname), nrec = 2, doprint = False):
+    """Given an input VCF on path invcf_file and a dictionary faidict
+    (produced by the fai2dict function) print the first nrec records' INFO
+    field and return the nrec'th record.
+    """
     i = 0
-    with open(invcf_file, "r") as f:
+    with open(invcf_file, "r") as f, open(outvcf_file, "w") as g:
         invcf = vcf.Reader(f)
+        outvcf = vcf.Writer(g, invcf)
         for r in invcf:
-            r.INFO["POS1"] = r.POS + fai[r.CHROM]
-            print(r.INFO)
+            r.INFO["OFFSETPOS"] = r.POS + faidict[r.CHROM]
+            outvcf.write_record(r)
+            if doprint:
+                print(r.INFO)
             if i >= nrec:
                 break
             i += 1
@@ -33,4 +41,4 @@ def print_records(invcf_file, fai, nrec = 2):
         return(r)
 
 if __name__ == "__main__":
-    print_records(invcfname, fai = fai2dict(faifname))
+    print_records(invcfname, doprint = True).INFO
