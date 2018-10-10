bdc24d77faa05e709706cbe0c95be18ca2c92de6
Attila Gulyas-Kovacs <attila.gulyas.kovacs@gmail.com>
2018-10-10 15:57:36 -0400

    more scripts turned into I/O filter

--- src/annotate1vcf.sh
+++ src/annotate1vcf.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-# annotate using vcflib a single VCF read from STDIN
+# Annotate using vcflib a single VCF.
+# Input VCF from STDIN, output VCF to STDOUT in uncompressed (default) or
+# compressed output type.
 
-usage="`realpath $0` invcf outvcf"
+usage="`realpath $0` < invcf > outvcf"
 
 PATH=/home/attila/tools/vcflib/bin:$PATH
 outputType=v
@@ -14,17 +16,13 @@ while getopts "O:" opt; do
 done
 shift $(($OPTIND - 1))
 
-invcf=$1
 SCRIPTDIR=$HOME/projects/bsm/src/
 REFERENCE="$REFSEQ"
 
-outvcf=$2
 
-# invcf and outvcf must not be the same file
-if test `realpath $invcf` == `realpath $outvcf`; then
-    echo $usage 1>&2; exit 1
-fi
-
+invcf=`tempfile`
+bcftools view -O v -o $invcf
+outvcf=`tempfile`
 OUT0=`tempfile`
 OUT1=`tempfile`
 OUT2=`tempfile`
@@ -51,22 +49,22 @@ tabix -s 1 -b 2 -e 2 -f $outvcf.vcfentropy_7.info.gz
 bgzip $outvcf.vcfentropy_15.info
 tabix -s 1 -b 2 -e 2 -f $outvcf.vcfentropy_15.info.gz
 
-echo "Annotate $invcf with the number of bases to the closest variant."
+1>&2 echo "Annotate $invcf with the number of bases to the closest variant."
 bcftools annotate -a $outvcf.vcfdistance.info.gz -c CHROM,POS,REF,ALT,BasesToClosestVariant -h$SCRIPTDIR/vcfdistance.info $OUT0.gz > $OUT1
 bgzip $OUT1
 tabix -p vcf $OUT1.gz
 
-echo "Annotate $invcf with the entropy of the reference sequence near the variant in a 7 bp window."
+1>&2 echo "Annotate $invcf with the entropy of the reference sequence near the variant in a 7 bp window."
 bcftools annotate -a $outvcf.vcfentropy_7.info.gz -c CHROM,POS,REF,ALT,EntropyLeft_7,EntropyCenter_7,EntropyRight_7 -h$SCRIPTDIR/vcfentropy_7.info $OUT1.gz > $OUT2
 bgzip $OUT2
 tabix -p vcf $OUT2.gz
 
-echo "Annotate $invcf with the entropy of the reference sequence near the variant in a 15 bp window."
+1>&2 echo "Annotate $invcf with the entropy of the reference sequence near the variant in a 15 bp window."
 bcftools annotate -a $outvcf.vcfentropy_15.info.gz -c CHROM,POS,REF,ALT,EntropyLeft_15,EntropyCenter_15,EntropyRight_15 -h$SCRIPTDIR/vcfentropy_15.info $OUT2.gz > $OUT3
 bgzip $OUT3
 tabix -p vcf $OUT3.gz
 
-echo "Left normalize $invcf"
-bcftools norm -o $outvcf -O $outputType -f $REFERENCE -D $OUT3.gz
+1>&2 echo "Left normalize $invcf"
+bcftools norm -O $outputType -f $REFERENCE -D $OUT3.gz
 
-rm $OUT0.* $OUT1.* $OUT2.* $OUT3.* $outvcf.*
+#rm $OUT0.* $OUT1.* $OUT2.* $OUT3.* $outvcf.* $invcf
