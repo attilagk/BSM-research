c41d3a0af3edce744a452b0477939ffe3beb9bdd
Attila Gulyas-Kovacs <attila.gulyas.kovacs@gmail.com>
2018-11-21 13:59:19 -0500

    Dx for sequenced-individuals

--- src/sequenced-individuals.sh
+++ src/sequenced-individuals.sh
@@ -4,10 +4,15 @@
 
 indivdir=/projects/bsm/data/dnalib
 indivs=$indivdir/cmc-individuals
+chesscsv=$indivdir/BSM_Project_Chess.csv
+sortedunique=`mktemp`
+cut -f1,12 -d, $chesscsv | sort -u > $sortedunique
 readsdir=/projects/bsm/reads
 for indiv in `cat $indivs`; do
-    indiv="$(echo $indiv | sed 's/CMC_//' | tr -d '_')"
-    if test $(find $readsdir -name "${indiv}*.fq*" | wc -w) -ne 0; then
-        echo $indiv
+    indivshort="$(echo $indiv | sed 's/CMC_//' | tr -d '_')"
+    if test $(find $readsdir -name "${indivshort}*.fq*" | wc -w) -ne 0; then
+        #echo $indiv
+        grep $indiv $sortedunique | tr ',' '\t'
     fi
 done
+rm $sortedunique
