09ea1fde0d0ce440ecd61542f6a512de7d78af94
Attila Gulyas-Kovacs <attila.gulyas.kovacs@gmail.com>
2018-12-04 00:05:03 -0500

    sequenced-samples.sh extended to create indiv-sample-fastq-names

--- src/sequenced-samples.sh
+++ src/sequenced-samples.sh
@@ -1,9 +1,15 @@
 #! /usr/bin/env bash
 
+# Given "sequenced-individuals" get sequenced samples and create indiv-sample-fastq-names
+
+# Must be run on Ada.  No command line arguments are required.
+
 case $HOSTNAME in
     ada) : ;;
     *) exit ;;
 esac
+
+# store sequenced samples in outfile
 
 outdir=/projects/bsm/attila/results/2018-09-12-sequenced-individuals
 outfile=$outdir/sequenced-samples
@@ -20,3 +26,23 @@ for indiv in $@; do
         sort -u | tr '\n' '\t'
     echo
 done > $outfile
+
+
+# for each indiv-sample store fastq file names in indiv-sample-fastq-names
+
+bamdir=/projects/bsm/alignments
+
+cut -f1 $outfile |
+    for indiv in `cat`; do
+        indivno_=$(echo $indiv | tr -d _ )
+        if test -d $bamdir/$indiv; then
+            :
+        else
+            mkdir $bamdir/$indiv
+            sed -n "/$indiv/ p" $outfile | cut -f3- |
+                for sample in `cat`; do
+                    find /projects/bsm/reads/ -name "*${indivno_}_${sample}*.fq.gz" > \
+                        $bamdir/$indiv/${indiv}_${sample}-fastq-names
+                done
+        fi
+    done
