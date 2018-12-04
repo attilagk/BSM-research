892b0785306ab28bd0840fca24dbe7de36df40a4
Attila Gulyas-Kovacs <attila.gulyas.kovacs@gmail.com>
2018-12-04 00:40:33 -0500

    progress with querying idnividuals

--- src/query-individual.sh
+++ src/query-individual.sh
@@ -11,12 +11,31 @@ rownames=$HOME/projects/bsm/tables/summary-rownames.csv
 # subject a.k.a. individual
 echo $indiv
 
-# BAMs
 bamdir=/projects/bsm/alignments/$indiv/
+# FASTQs
+for sample in $(sed -n "/fastq/ { s/.fastq//; p }" $rownames); do
+    fqnames=$bamdir/${indiv}_${sample}-fastq-names
+    if test -f $fqnames; then
+        echo 1
+    else
+        echo 0
+    fi
+done
+# BAMs
 for sample in $(sed -n "/fastq/ { s/.fastq//; p }" $rownames); do
     bam=$bamdir/${indiv}_${sample}.bam
     bai=$bamdir/${indiv}_${sample}.bam.bai
     if test -f $bam && test -f $bai; then
+        echo 1
+    else
+        echo 0
+    fi
+done
+
+vcfdir=/projects/bsm/calls/$indiv/snvs
+for caller in $(sed -n "/vcf/ { s/.vcf//; p }" $rownames); do
+    vcf=$vcfdir/$caller.vcf.gz
+    if test -f $vcf; then
         echo 1
     else
         echo 0
