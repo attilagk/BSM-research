300ca01da91dd307092252f2b8b787a13c03ec0a
Attila Gulyas-Kovacs <attila.gulyas.kovacs@gmail.com>
2018-12-04 02:03:14 -0500

    query-individual.sh progress

--- src/query-individual.sh
+++ src/query-individual.sh
@@ -5,39 +5,54 @@ case $HOSTNAME in
     *) exit ;;
 esac
 
-indiv=$1
-rownames=$HOME/projects/bsm/tables/summary-rownames.csv
+tablesdir=$HOME/projects/bsm/tables
+rownames=$tablesdir/summary-rownames.csv
+summarydir=$tablesdir/summary
+summaryfile=$tablesdir/summary.tsv
+if ! test -d $summarydir; then
+    mkdir $summarydir
+fi
+seqind=/projects/bsm/attila/results/2018-09-12-sequenced-individuals/sequenced-individuals
 
-# subject a.k.a. individual
-echo $indiv
+do1indiv () {
+    # subject a.k.a. individual
+    indiv=$1
+    bamdir=/projects/bsm/alignments/$indiv/
+    # FASTQs
+    for sample in $(sed -n "/fastq/ { s/.fastq//; p }" $rownames); do
+        fqnames=$bamdir/${indiv}_${sample}-fastq-names
+        if test -f $fqnames; then
+            echo 1
+        else
+            echo 0
+        fi
+    done
+    # BAMs
+    for sample in $(sed -n "/fastq/ { s/.fastq//; p }" $rownames); do
+        bam=$bamdir/${indiv}_${sample}.bam
+        bai=$bamdir/${indiv}_${sample}.bam.bai
+        if test -f $bam && test -f $bai; then
+            echo 1
+        else
+            echo 0
+        fi
+    done
+    # VCFs
+    vcfdir=/projects/bsm/calls/$indiv/snvs
+    for caller in $(sed -n "/vcf/ { s/.vcf//; p }" $rownames); do
+        vcf=$vcfdir/$caller.vcf.gz
+        if test -f $vcf; then
+            echo 1
+        else
+            echo 0
+        fi
+    done
+}
 
-bamdir=/projects/bsm/alignments/$indiv/
-# FASTQs
-for sample in $(sed -n "/fastq/ { s/.fastq//; p }" $rownames); do
-    fqnames=$bamdir/${indiv}_${sample}-fastq-names
-    if test -f $fqnames; then
-        echo 1
-    else
-        echo 0
-    fi
+cp $rownames $summaryfile
+indivs="$(cut -f1 $seqind)"
+for ind in $indivs; do
+    do1indiv $ind > $summarydir/$ind
 done
-# BAMs
-for sample in $(sed -n "/fastq/ { s/.fastq//; p }" $rownames); do
-    bam=$bamdir/${indiv}_${sample}.bam
-    bai=$bamdir/${indiv}_${sample}.bam.bai
-    if test -f $bam && test -f $bai; then
-        echo 1
-    else
-        echo 0
-    fi
-done
-
-vcfdir=/projects/bsm/calls/$indiv/snvs
-for caller in $(sed -n "/vcf/ { s/.vcf//; p }" $rownames); do
-    vcf=$vcfdir/$caller.vcf.gz
-    if test -f $vcf; then
-        echo 1
-    else
-        echo 0
-    fi
-done
+cd $summarydir
+paste $rownames $indivs
