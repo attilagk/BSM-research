08b99c7b6b6863abf25c95894e49c40458db5a7c
Attila Gulyas-Kovacs <attila.gulyas.kovacs@gmail.com>
2018-12-11 15:33:09 -0500

    NDA python validation tool test0

--- ndar/benchmark/vt-java/submit.sh
+++ ndar/benchmark/vt-java/submit.sh
@@ -1,15 +1,17 @@
 #! /usr/bin/env bash
 
-d=/home/attila/projects/bsm/ndar/ref-tissue
+d=/home/attila/projects/bsm/ndar/benchmark
 f1=$d/nichd_btb02-chess-benchmark-2018-12-10.csv
 f2=$d/genomics_subject02-chess-benchmark-2018-12-10.csv
 f3=$d/genomics_sample03-chess-benchmark-2018-12-10.csv
+
+file $f1 $f2 $f3; exit
 
 # Oracle 8
 # /usr/lib/jvm/java-8-oracle/bin/java \
     # OpenJDK
 # java \
-    /usr/lib/jvm/java-8-oracle/bin/java \
+/usr/lib/jvm/java-8-oracle/bin/java \
     -jar /opt/validationtool/current/validationtool-console-client-1.10.2.jar \
     --validationAPI https://ndar.nih.gov/api/validationtool/v2 \
     --collectionId 2458 \
---
+++ ndar/benchmark/vt-python/test0/submit.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+vtcmd \
+    --username attilagk \
+    --password Chesslab13 \
+    --collectionID 2458 \
+    --title test0 \
+    --buildPackage \
+    --submitPackage \
+    --description "Testing submission" \
+    < test0-fastq-names
