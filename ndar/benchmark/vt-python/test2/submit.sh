d11a8a421f4232ea351a98def5dd9495c8ef16f6
Attila Gulyas-Kovacs <attila.gulyas.kovacs@gmail.com>
2018-12-12 14:32:29 -0500

    ndar validation tests

--- ndar/benchmark/vt-python/test2/submit.sh
+++ ndar/benchmark/vt-python/test2/submit.sh
@@ -2,17 +2,17 @@
 
 #d=/home/attila/projects/bsm/ndar/benchmark/vt-python/test0
 d=`dirname $0`
-echo $d; exit
 f1=nichd_btb02.csv
 f2=genomics_subject02.csv
 f3=genomics_sample03.csv
 
 vtcmd \
+	$f1 $f2 $f3 \
     --username attilagk \
     --password Chesslab13 \
-    --manifestPath $d \
+    --listDir $d \
     --collectionID 2458 \
     --title test0 \
     --buildPackage \
     --description "Testing submission" \
-    $f1 $f2 $f3
+    
