c94369f927633e61773fb00342b4c7990fc40f5c
Attila Gulyas-Kovacs <attila.gulyas.kovacs@gmail.com>
2018-12-20 12:00:33 -0500

    nda_tools updated; relative paths in genomics_sample03.csv

--- ndar/benchmark/vt-python/real-data/submit.sh
+++ ndar/benchmark/vt-python/real-data/submit.sh
@@ -2,16 +2,16 @@
 
 #d=/home/attila/projects/bsm/ndar/benchmark/vt-python/test0
 d=`dirname $0`
-f1=nichd_btb02.csv
-f2=genomics_subject02.csv
-f3=genomics_sample03.csv
+f1=$d/nichd_btb02.csv
+f2=$d/genomics_subject02.csv
+f3=$d/genomics_sample03.csv
 
 vtcmd \
 	$f1 $f2 $f3 \
     -u attilagk \
     -p Chesslab13 \
-    -l /projects/bsm /projects/bsm/reads/2018-01-10-Benchmark-DV-X10 \
-    -c 2458 \
+    -l /projects/bsm/alignments/ceph-benchmark/ /projects/bsm/reads/2018-01-10-Benchmark-DV-X10/ \
+    -a BSMN-S3 \
     -t Benchmark \
     -d "FASTQs and BAMs for Benchmark (CEPH/Utah DNA mixes), Chess lab" \
     -b
