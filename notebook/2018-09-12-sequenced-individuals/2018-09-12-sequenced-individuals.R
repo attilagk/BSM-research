31b2383b760a7c2f5c827836df00a5a506569a0a
Attila Gulyas-Kovacs <attila.gulyas.kovacs@gmail.com>
2019-02-06 18:36:53 -0500

    workflow operations for summary figure for sequenced individuals

--- notebook/2018-09-12-sequenced-individuals/2018-09-12-sequenced-individuals.R
+++ notebook/2018-09-12-sequenced-individuals/2018-09-12-sequenced-individuals.R
@@ -3,21 +3,30 @@ reshape.summ <- function(sm) {
     long$done <- sapply(seq.int(dim(long)[1]),
                         function(y) sm[as.character(long[y, 1]), as.character(long[y, 2])])
     names(long)[1:2] <- c("workflow", "individuals")
+    long$operation <- long$workflow
+    long$operation <- sub(".*\\.fastq", "Sequencing", long$operation)
+    long$operation <- sub(".*\\.bam", "Mapping", long$operation)
+    long$operation <- sub(".*\\.vcf", "Calling", long$operation)
+    long$operation <- factor(long$operation, ordered = TRUE, levels = c("Sequencing", "Mapping", "Calling"))
+    long[long$operation == "Mapping" & long$done == 1 & ! is.na(long$done), "done"] <- 2
+    long[long$operation == "Calling" & long$done == 1 & ! is.na(long$done), "done"] <- 3
     return(long)
 }
 
-my.levelplot <- function(df, my.col = c("white", "blue"), indiv.as.columns = FALSE) {
+my.levelplot <- function(df, my.col = c("gray", "darkgreen", "red", "blue"), indiv.as.columns = FALSE) {
     xs.tr <- rev # axis transformation function
     dfl <- lapply(df, reshape.summ)
     dfl <- do.call(rbind, lapply(names(dfl), function(y) data.frame(group = y, dfl[[y]])))
     ncols <- sapply(df, ncol)
     nrows <- sapply(df, nrow)
+    nops <- length(levels(dfl$operation)) # number of operations
     rlim <- cumsum(ncols)
     llim <- rlim - ncols + 1
     limits <- lapply(seq_along(llim), function(y) c(llim[y], rlim[y]))
     at <- lapply(limits, function(l) seq(from = l[1], to = l[2], by = 1))
     labels <- lapply(at, function(y) levels(dfl$individuals)[y])
-    lp.1 <- levelplot(done ~ workflow * individuals | group, data = dfl, col.regions = my.col, colorkey = FALSE,
+    #labels <- unlist(lapply(labels, function(y) c(list(y), rep(list(rep("", length(y))), nops - 1))), recursive = FALSE)
+    lp.1 <- levelplot(done ~ workflow * individuals | group , data = dfl, col.regions = my.col, colorkey = FALSE,
               scales = list(x = list(rot = 90),
                             y = list(alternating = FALSE, relation = "free", limits = limits, labels = labels, at = at)),
               xlab = "", ylab = "", layout = c(1, length(df)), between = list(y = 0.5))
@@ -28,6 +37,8 @@ my.levelplot <- function(df, my.col = c("white", "blue"), indiv.as.columns = FAL
     lp.1 <- resizePanels(lp.1, h = ncols)
     lp.2 <- resizePanels(lp.2, w = ncols)
     lp <- if(indiv.as.columns) lp.2 else lp.1
+    lp <- update(lp, auto.key = list(text = levels(dfl$operation), rectangles = TRUE, points = FALSE),
+                 par.settings = list(superpose.polygon = list(col = my.col[-1])))
     print(lp)
     return(dfl)
 }
