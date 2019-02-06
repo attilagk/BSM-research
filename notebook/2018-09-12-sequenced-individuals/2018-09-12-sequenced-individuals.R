7bea2248f1846d0401c8cab42760759926c536bf
Attila Gulyas-Kovacs <attila.gulyas.kovacs@gmail.com>
2019-02-06 15:50:29 -0500

    prettified levelplot summary for sequenced individuals

--- notebook/2018-09-12-sequenced-individuals/2018-09-12-sequenced-individuals.R
+++ notebook/2018-09-12-sequenced-individuals/2018-09-12-sequenced-individuals.R
@@ -5,3 +5,29 @@ reshape.summ <- function(sm) {
     names(long)[1:2] <- c("workflow", "individuals")
     return(long)
 }
+
+my.levelplot <- function(df, my.col = c("white", "blue"), indiv.as.columns = FALSE) {
+    xs.tr <- rev # axis transformation function
+    dfl <- lapply(df, reshape.summ)
+    dfl <- do.call(rbind, lapply(names(dfl), function(y) data.frame(group = y, dfl[[y]])))
+    ncols <- sapply(df, ncol)
+    nrows <- sapply(df, nrow)
+    rlim <- cumsum(ncols)
+    llim <- rlim - ncols + 1
+    limits <- lapply(seq_along(llim), function(y) c(llim[y], rlim[y]))
+    at <- lapply(limits, function(l) seq(from = l[1], to = l[2], by = 1))
+    labels <- lapply(at, function(y) levels(dfl$individuals)[y])
+    lp.1 <- levelplot(done ~ workflow * individuals | group, data = dfl, col.regions = my.col, colorkey = FALSE,
+              scales = list(x = list(rot = 90),
+                            y = list(alternating = FALSE, relation = "free", limits = limits, labels = labels, at = at)),
+              xlab = "", ylab = "", layout = c(1, length(df)), between = list(y = 0.5))
+    lp.2 <- levelplot(done ~ individuals * xs.tr(workflow) | group, data = dfl, col.regions = my.col, colorkey = FALSE,
+              scales = list(x = list(rot = 90, alternating = FALSE, relation = "free", limits = limits, labels = labels, at = at),
+                            y = list(labels = levels(dfl$workflow), at = xs.tr(seq_len(nrows[1])))),
+              xlab = "", ylab = "", layout = c(length(df), 1), between = list(x = 0.5))
+    lp.1 <- resizePanels(lp.1, h = ncols)
+    lp.2 <- resizePanels(lp.2, w = ncols)
+    lp <- if(indiv.as.columns) lp.2 else lp.1
+    print(lp)
+    return(dfl)
+}
