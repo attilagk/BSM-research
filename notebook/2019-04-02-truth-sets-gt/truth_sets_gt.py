aa97a373e9338ffaf65192c9ab9ec90f3e5211b0
Attila Gulyas-Kovacs <attila.gulyas.kovacs@gmail.com>
2019-04-03 17:07:46 -0400

    Truth sets for genotypes completed and ran successfully

--- notebook/2019-04-02-truth-sets-gt/truth_sets_gt.py
+++ notebook/2019-04-02-truth-sets-gt/truth_sets_gt.py
@@ -1,8 +1,10 @@
+#! /usr/bin/env python3
 import pandas as pd
 import numpy as np
 import subprocess
 import os
 import re
+import shutil
 
 def read_aaf_of_gt(csvpath='/home/attila/projects/bsm/results/2019-03-12-prec-recall-design/aaf_of_gt.csv'):
     names =  ['genotype', 'mix1', 'mix2', 'mix3']
@@ -10,7 +12,7 @@ def read_aaf_of_gt(csvpath='/home/attila/projects/bsm/results/2019-03-12-prec-re
     aaf_of_gt = pd.read_csv(csvpath, names=names, dtype=dtype, header=0)
     return(aaf_of_gt)
 
-def bcftools_isec_args(gt,
+def bcftools_isec_gt(gt,
         indir='/home/attila/projects/bsm/results/2019-03-18-truth-sets/chr22/snp/truthset/individuals',
         outdir='/home/attila/projects/bsm/results/2019-03-18-truth-sets/chr22/snp/truthset/genotypes'):
     def make_flist():
@@ -23,7 +25,6 @@ def bcftools_isec_args(gt,
         res = [indir + os.sep + y + '.vcf.gz' for y in res]
         return(res)
     flist = make_flist()
-    outfpath = outdir + os.sep + gt
     nstr = re.sub('2', '1', gt)
     pdirpath = outdir + os.sep + gt
     # arguments for bcftools isec
@@ -32,12 +33,22 @@ def bcftools_isec_args(gt,
     c_opt = ['-c', 'none']
     n_opt = ['-n~' + nstr]
     args0 = bcftool0 + p_opt + c_opt + n_opt + flist
-    # arguments for copying VCF to keep
+    # arguments for copying and gzipping the output VCF to keep
+    outfpath = pdirpath + '.vcf.gz'
     bcftool1 = ['bcftools', 'view']
     O_opt = ['-Oz']
-    o_opt = ['-o', pdirpath + '.vcf.gz']
+    o_opt = ['-o', outfpath]
     vcf2keep = [pdirpath + os.sep + '000' + str(nstr.find('1')) + '.vcf']
     args1 = bcftool1 + O_opt + o_opt + vcf2keep
-    return((args0, args1))
+    # arguments for indexing gzipped output VCF
+    args2 = ['bcftools', 'index', '-t', outfpath]
+    # execute
+    subprocess.run(args0)
+    subprocess.run(args1)
+    subprocess.run(args2)
+    shutil.rmtree(pdirpath)
+    return((args0, args1, args2))
 
-# bcftools isec -n~1100 -c all A.vcf.gz B.vcf.gz C.vcf.gz D.vcf.gz
+if __name__ == '__main__':
+    import sys
+    bcftools_isec_gt(gt=sys.argv[1], indir=sys.argv[2], outdir=sys.argv[3])
