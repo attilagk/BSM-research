fb3615116557b6224315267a8a4e2bcba238595b
Attila Gulyas-Kovacs <attila.gulyas.kovacs@gmail.com>
2019-11-15 16:08:38 -0500

    Running PON VCF scipt

--- notebook/2019-11-13-panel-of-normals/tnseq_pon.py
+++ notebook/2019-11-13-panel-of-normals/tnseq_pon.py
@@ -13,7 +13,7 @@ REFSEQ = '/projects/shared/refgenome/GRCh37/dna/hs37d5.fa'
 sentieon_executable = '/opt/sentieon-genomics/current/bin/sentieon'
 
 
-def bam2pon(bam='/projects/bsm/alignments/MSSM_065/MSSM_065_NeuN_pl.bam',
+def bam2pon(bam='/projects/bsm/alignments/PITT_118/PITT_118_NeuN_pl.bam',
         nthreads=NUMBER_THREADS, refseq=REFSEQ, algo='TNhaplotyper'):
     '''
     Create a panel of normal (PON) VCF based on a single BAM
@@ -33,14 +33,29 @@ def bam2pon(bam='/projects/bsm/alignments/MSSM_065/MSSM_065_NeuN_pl.bam',
     vcf = outdir + os.path.sep + vcf
     if not os.path.exists(outdir):
         os.mkdir(outdir)
-    args = [sentieon_executable, 'driver', '--interval', '22:20000000-25000000',
-    #args = [sentieon_executable, 'driver',
+    #args = [sentieon_executable, 'driver', '--interval', '22:20000000-25000000', # for testing
+    args = [sentieon_executable, 'driver',
             '-t', nthreads, '-r', refseq, '-i', bam, '--algo', algo, '--detect_pon', vcf]
     proc = subprocess.run(args)
-    return(proc)
+    def index_pon_vcf():
+        '''
+        Replace PON VCF with its gzipped, indexed, version
+        '''
+        vcfgz = vcf + '.gz'
+        args0 = ['bcftools', 'view', '-O', 'z', '-o', vcfgz, vcf]
+        proc0 = subprocess.run(args0)
+        args1 = ['bcftools', 'index', '--tbi', vcfgz]
+        proc1 = subprocess.run(args1)
+        if proc1.returncode == 0:
+            os.remove(vcf)
+            os.remove(vcf + '.idx')
+        return(vcfgz)
+    val = index_pon_vcf()
+    return(val)
+
 
 def all_bam2pon_merge(bamlist=glob.glob('/projects/bsm/alignments/[MP][SI][ST][MT]_*/*.bam'),
-        ponvcf=resultdir + os.path.sep + 'bsm-cmc-pon.vcf'):
+        ponvcf=resultdir + os.path.sep + 'bsm-cmc-pon.vcf.gz'):
     '''
     Create and merge PON VCFs for a list of BAMs
 
@@ -51,11 +66,11 @@ def all_bam2pon_merge(bamlist=glob.glob('/projects/bsm/alignments/[MP][SI][ST][M
     Value:
     a completed process (see subprocess)
     '''
-    bamlist = ['/projects/bsm/alignments/PITT_118/PITT_118_NeuN_mn.bam',
-             '/projects/bsm/alignments/PITT_118/PITT_118_NeuN_pl.bam']
-    #proc = [bam2pon(b) for b in bamlist]
+    vcflist = [bam2pon(b) for b in bamlist]
     outdir = resultdir + os.path.sep + 'VCFs'
-    vcflist = glob.glob(outdir + os.path.sep + '*.vcf')
-    args = ['bcftools', 'merge', '-m', 'all', '--force-samples', '-o', ponvcf] + vcflist
-    #proc = subprocess.run(args)
-    return(args)
+    vcflist = glob.glob(outdir + os.path.sep + '*.vcf.gz')
+    args0 = ['bcftools', 'merge', '-m', 'all', '--force-samples', '-Oz', '-o', ponvcf] + vcflist
+    proc0 = subprocess.run(args0)
+    args1 = ['bcftools', 'index', '--tbi', ponvcf] + vcflist
+    proc1 = subprocess.run(args1, capture_output=True)
+    return(proc1)
