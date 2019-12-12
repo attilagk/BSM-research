df3dd0570b58023796035fbc5694969d6783ad6a
Attila Gulyas-Kovacs <attila.gulyas.kovacs@gmail.com>
2019-12-12 14:59:12 -0500

    New script to list sequenced samples' info: Institution Dissection ID, Individual ID,...

--- src/sequenced-individuals.sh
+++ src/sequenced-individuals.sh
@@ -5,14 +5,14 @@
 indivdir=/projects/bsm/data/dnalib
 indivs=$indivdir/cmc-individuals
 chesscsv=$indivdir/BSM_Project_Chess.csv
+outfile=/projects/bsm/attila/results/2018-09-12-sequenced-individuals/sequenced-individuals
 sortedunique=`mktemp`
 cut -f1,12 -d, $chesscsv | sort -u > $sortedunique
 readsdir=/projects/bsm/reads
 for indiv in `cat $indivs`; do
     indivshort="$(echo $indiv | sed 's/CMC_//' | tr -d '_')"
     if test $(find $readsdir -name "${indivshort}*.fq*" | wc -w) -ne 0; then
-        #echo $indiv
         grep $indiv $sortedunique | tr ',' '\t'
     fi
-done
+done > $outfile
 rm $sortedunique
