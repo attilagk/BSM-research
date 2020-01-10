c7fc55fbf8cb2cdc0fa3194f3438fbdcde69455b
Attila Gulyas-Kovacs <attila.gulyas.kovacs@gmail.com>
2020-01-10 14:05:43 -0500

    third attempt at 2019-12-09-upload-to-ndar

--- src/submit2nda.py
+++ src/submit2nda.py
@@ -2,8 +2,8 @@
 
 import subprocess
 
-def submit(btb, gsub, gsam, title, build=False, user='andrewchess', pw='Bern1e2017',
-        collection='2965', lib='/projects/bsm/'):
+def submit(btb, gsub, gsam, title, description, build=False, user='andrewchess', pw='Bern1e2017',
+        collection='2965', lib='/projects/bsm/', alternateEndpoint='BSMN-S3'):
     '''
     Validate and submit data to NDA
 
@@ -12,17 +12,18 @@ def submit(btb, gsub, gsam, title, build=False, user='andrewchess', pw='Bern1e20
     gsub: genomic subjects file
     gsam: genomic samples file
     title: submission title
+    description: submission description
     build: whether to build package
     user: NDA user name
     pw: NDA password
     collection: NDA collection
     lib: data file directory
+    alternateEndpoint: alternate endpoint
 
     Value: the process object
     '''
-    description = 'Unmapped and mapped reads from bulk sequencing; NIH U01MH106891; Chess Lab, Mount Sinai, New York'
     args = ['/home/attila/.local/bin/vtcmd', btb, gsub, gsam]
-    args += ['-u', user, '-p', pw, '-c', collection, '-l', lib, '-t', title]
+    args += ['-u', user, '-p', pw, '-c', collection, '-l', lib, '-a', alternateEndpoint, '-t', title, '-d', description]
     if build:
         args += ['-b']
     proc = subprocess.run(args, capture_output=True)
@@ -36,6 +37,8 @@ if __name__ == '__main__':
     parser.add_argument('gsam', help='genomic samples file')
     parser.add_argument('-t', '--title', help='submission title',
             default='Chess lab data')
+    parser.add_argument('-d', '--description', help='description',
+            default='Illumina reads in FASTQ and BAM')
     parser.add_argument('-u', '--user', help='NDA user name',
             default='andrewchess')
     parser.add_argument('-p', '--password', help='password',
@@ -44,10 +47,12 @@ if __name__ == '__main__':
             default='2965')
     parser.add_argument('-l', '--lib', help='data file directory',
             default='/projects/bsm/')
+    parser.add_argument('-a', '--alternateEndpoint', help='alternate Endpoint',
+            default='BSMN-S3')
     parser.add_argument('-b', '--build', help='build package',
             action="store_true")
     args = parser.parse_args()
     proc = submit(btb=args.btb, gsub=args.gsub, gsam=args.gsam,
-            title=args.title, build=args.build, user=args.user, pw=args.password,
-            collection=args.collection, lib=args.lib)
+            title=args.title, description=args.description, build=args.build, user=args.user, pw=args.password,
+            collection=args.collection, lib=args.lib, alternateEndpoint=args.alternateEndpoint)
     print(proc.stdout.decode('utf-8'))
