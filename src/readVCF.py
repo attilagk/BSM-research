d5af3f5eca6396f961d0dca869a0a822eef71e16
Attila <attila.gulyas.kovacs@gmail.com>
2020-06-12 13:15:47 -0400

    towards reading multiple VCFs and annotating with clinical metadata

--- src/readVCF.py
+++ src/readVCF.py
@@ -15,6 +15,20 @@ def make_formatstr(annotlist):
     formatstr = '%' + '\t%'.join(annotlist) + '\n'
     return(formatstr)
 
+def sample_fromVCF(vcfpath):
+    cmd = ['bcftools', 'view', '-h', vcfpath]
+    p =  subprocess.run(cmd, capture_output=True)
+    colnames = p.stdout.splitlines()[-1]
+    sample = re.sub('^.*\t([^\t]+)$', '\\1', colnames.decode('utf-8'))
+    return(sample)
+
+def convert_sample(bsm_sample):
+    s = re.sub('^((MSSM|PITT)_([0-9]+))_(.*)$', '\\1:\\4', bsm_sample)
+    l = s.split(':')
+    Indiv_ID = 'CMC_' + l[0]
+    Tissue = l[1]
+    return((Indiv_ID, Tissue))
+
 def readVCF(vcfpath, annotlist=read_annotlist()):
     formatstr = make_formatstr(annotlist)
     colnames = [y.replace('INFO/', '') for y in annotlist]
@@ -22,3 +36,6 @@ def readVCF(vcfpath, annotlist=read_annotlist()):
     p =  subprocess.run(cmd, capture_output=True)
     df = pd.read_csv(io.BytesIO(p.stdout), sep='\t', names=colnames)
     return(df)
+
+def readVCFs(vcflistf):
+    pass
