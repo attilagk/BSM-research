3a0e39c9763c9f34c8592d592866186f4291f3d2
Attila <attila.gulyas.kovacs@gmail.com>
2020-06-12 16:07:33 -0400

    Reorganized filtered VCFs

--- src/readVCF.py
+++ src/readVCF.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import numpy as np
 import subprocess
 import io
 import re
+import synapseclient
 
 def read_annotlist(annotpath='/home/attila/projects/bsm/tables/VCF-HC.annotations', withFORMAT=False):
     with open(annotpath) as f:
@@ -25,9 +27,9 @@ def sample_fromVCF(vcfpath):
 def convert_sample(bsm_sample):
     s = re.sub('^((MSSM|PITT)_([0-9]+))_(.*)$', '\\1:\\4', bsm_sample)
     l = s.split(':')
-    Indiv_ID = 'CMC_' + l[0]
-    Tissue = l[1]
-    return((Indiv_ID, Tissue))
+    indiv_id = 'CMC_' + l[0]
+    tissue = l[1]
+    return((indiv_id, tissue))
 
 def readVCF(vcfpath, annotlist=read_annotlist()):
     formatstr = make_formatstr(annotlist)
@@ -37,5 +39,23 @@ def readVCF(vcfpath, annotlist=read_annotlist()):
     df = pd.read_csv(io.BytesIO(p.stdout), sep='\t', names=colnames)
     return(df)
 
-def readVCFs(vcflistf):
-    pass
+def add_clinical(vcfpath, clinical):
+    vcf = readVCF(vcfpath)
+    sample = sample_fromVCF(vcfpath)
+    indiv_id = convert_sample(sample)[0]
+    df = clinical.loc[[indiv_id]]
+    df = df.iloc[np.zeros(vcf.shape[0]), :]
+    df.index = vcf.index
+    outvcf = pd.concat([vcf, df], axis=1)
+    return(outvcf)
+
+def readVCFs(vcflistpath='/big/results/bsm/calls/filtered-vcfs.tsv'):
+    # CMC_Human_clinical_metadata.csv
+    syn = synapseclient.login()
+    wdir='/tmp'
+    cmc_clinical_syn = syn.get('syn2279441', downloadLocation=wdir, ifcollision='overwrite.local')
+    cmc_clinical = pd.read_csv(cmc_clinical_syn.path, index_col='Individual ID')
+    # TODO
+    add_clinical('/big/results/bsm/2020-06-10-chromatin-state/MSSM_109_brain.ploidy_50.filtered-annot.vcf',
+            clinical)
+    return(cmc_clinical)
