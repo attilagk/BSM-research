7953654528adb0b2dc693759e3d03ed6a9250a32
Attila Jones <attila.g.jones@gmail.com>
2020-07-27 23:30:01 -0400

    count occurrence of cytogenic bands in szdb's CNV table

--- notebook/2020-07-24-szdb/szdb.py
+++ notebook/2020-07-24-szdb/szdb.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def expandCNVrow(CNV, hgcb, corr_cb, ix):
     row = CNV.iloc[[ix], :]
-    chromosome = row.loc[row.index[0], 'chromosome']
+    chromosome = row.loc[row.index[0], 'chrom']
     start_cb = row.loc[row.index[0], 'start cytoband']
     # correct cytoband name if necessary to make it compatible with hgcb
     if (chromosome, start_cb) in corr_cb.index:
@@ -12,7 +12,7 @@ def expandCNVrow(CNV, hgcb, corr_cb, ix):
     if (chromosome, end_cb) in corr_cb.index:
         end_cb = corr_cb.loc[[(chromosome, end_cb)], 'corr end'][0][1]
     # get all cytobands for the given chromosome from hgcb
-    hgcb = hgcb.loc[hgcb['chromosome'] == chromosome, :]
+    hgcb = hgcb.loc[hgcb['chrom'] == chromosome, :]
     name = list(hgcb['name'])
     # find the indices of the start and end cytobands
     hgcb_ix = list((name.index(start_cb), name.index(end_cb)))
@@ -21,10 +21,10 @@ def expandCNVrow(CNV, hgcb, corr_cb, ix):
     # When the start and end band are the same the expansion factor is trivially 1.
     row = row.iloc[np.zeros(np.diff(hgcb_ix) + 1, dtype=int), :]
     hgcb_chunk = hgcb.iloc[range(hgcb_ix[0], hgcb_ix[1] + 1), :]
-    hgcb_chunk.index = row.index
+    row.index = hgcb_chunk.index
     # Now take info from hgcb to the CNV row
     row['hg cytoband'] = hgcb_chunk['name']
-    row['hg chr cytoband'] = [(c, n) for c, n in zip(hgcb_chunk['chromosome'], hgcb_chunk['name'])]
+    row['hg chr cytoband'] = [(c, n) for c, n in zip(hgcb_chunk['chrom'], hgcb_chunk['name'])]
     row['start'] = hgcb_chunk['start']
     row['end'] = hgcb_chunk['end']
     row.drop(columns=['start cytoband', 'end cytoband'], inplace=True)
@@ -48,11 +48,29 @@ def expandCNV(CNV, hgcb, corr_cb):
     return(CNV)
 
 
-def summarizeCNV(CNV):
-    gb = CNV.groupby(['chromosome', 'hg cytoband'])
+def bedifyCNV(CNV, hgcb):
+    '''
+    Count occurrence of each cytogenic band in szdb expanded CNV table and then bedify
+
+    Arguments
+    CNV: expanded CNV returned by expandCNV
+    hgcb: as above
+
+    Value: the bedified CNV
+    '''
+    gb = CNV.groupby(by='hg chr cytoband')
     first = gb.first()[['start', 'end']]
     chrom = pd.DataFrame({'chrom': [y[0] for y in first.index]})
     chrom.index = first.index
     count = pd.DataFrame({'count': gb.size()})
-    val = pd.concat([chrom, first, count], axis=1)
+    present = pd.concat([chrom, first, count], axis=1)
+    # some odds and ends for present
+    present.index = pd.MultiIndex.from_tuples(present.index)
+    # add missing zero count cyto bands
+    missing = hgcb.loc[list(set(hgcb.index) - set(present.index)), ['chrom', 'start', 'end']]
+    missing['count'] = 0
+    val = pd.concat([present, missing])
+    categories = [str(y) for y in range(1, 23)] + list('XY')
+    val['chrom'] = pd.Categorical(val['chrom'], ordered=True, categories=categories)
+    val.sort_values(by=['chrom', 'start'], inplace=True)
     return(val)
