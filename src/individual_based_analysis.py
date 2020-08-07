ebafbe94099a807291a479e22385084c6f36ba2d
Attila Jones <attila.g.jones@gmail.com>
2020-08-07 18:43:42 -0400

    Cleaining data

---
+++ src/individual_based_analysis.py
@@ -0,0 +1,17 @@
+import readVCF
+import pandas as pd
+import statsmodels.api as sm
+
+selvars = ['AntipsychAtyp', 'Alcohol', 'AntipsychTyp', 'Ethnicity']
+cmc_clinical_path = '/big/results/bsm/2020-08-05-cmc-clinical/CMC_Human_clinical_metadata.csv'
+ancestry_path='/home/attila/projects/bsm/resources/cmc-ancestry/CMC_MSSM-Penn-Pitt_DNA_GENOTYPE_ANCESTRY_GemTools.tsv'
+
+def clean_clinical(calls, remove_ancestry=False):
+    clin = pd.read_csv(cmc_clinical_path)
+    selvars = list(clin.columns)
+    todrop = set(clin.columns) - set(selvars)
+    if remove_ancestry:
+        ancestry = pd.read_csv(ancestry_path, sep='\t', index_col='Individual_ID')
+        todrop = todrop.union(set(ancestry.columns))
+    calls = calls.drop(columns=todrop)
+    return(calls)
