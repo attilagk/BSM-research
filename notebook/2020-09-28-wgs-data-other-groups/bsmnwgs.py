9ee88b1eebe1ba11ed25db900c4ff1447dab2eef
Attila Jones <attila.g.jones@gmail.com>
2020-09-30 16:06:13 +0200

    Number of control samples in BSMN completed

--- notebook/2020-09-28-wgs-data-other-groups/bsmnwgs.py
+++ notebook/2020-09-28-wgs-data-other-groups/bsmnwgs.py
@@ -1,2 +1,23 @@
 import synapseclient
+import pandas as pd
+import os.path
 
+syn = synapseclient.login()
+wdir = '/home/attila/projects/bsm/results/2020-09-28-wgs-data-other-groups/'
+
+def foo():
+    return('Hello World!')
+
+def get_manifest(synid):
+    e = syn.get(synid, downloadLocation=wdir + os.path.sep + 'downloads', ifcollision='overwrite.local')
+    manifest = pd.read_csv(e.path, skiprows=1)
+    #manifest['project'] = project
+    return(manifest)
+
+def get_manifests(synids, project, maniftype='genomics_subject02'):
+    l = [get_manifest(s) for s in synids]
+    manifest = pd.concat(l, axis=0)
+    manifest['project'] = project
+    fpath = wdir + os.path.sep + project + '-' + maniftype + '.csv'
+    manifest.to_csv(fpath)
+    return(manifest)
