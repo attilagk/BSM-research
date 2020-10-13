02f09d960b00bc3f7266368e31c5d38ee958e6cb
Attila Jones <attila.g.jones@gmail.com>
2020-10-12 20:21:45 -0400

    begun presentation for Walsh lab meeting

--- notebook/2020-09-28-wgs-data-other-groups/bsmnwgs.py
+++ notebook/2020-09-28-wgs-data-other-groups/bsmnwgs.py
@@ -5,13 +5,9 @@ import os.path
 syn = synapseclient.login()
 wdir = '/home/attila/projects/bsm/results/2020-09-28-wgs-data-other-groups/'
 
-def foo():
-    return('Hello World!')
-
 def get_manifest(synid):
     e = syn.get(synid, downloadLocation=wdir + os.path.sep + 'downloads', ifcollision='overwrite.local')
     manifest = pd.read_csv(e.path, skiprows=1)
-    #manifest['project'] = project
     return(manifest)
 
 def get_manifests(synids, project, maniftype='genomics_subject02'):
