a6cabe33b860d06e35cc62adf0279df064d6eff7
Attila Jones <attila.g.jones@gmail.com>
2020-11-18 20:30:11 -0500

    functional vars overlapping SCZ GWAS: summary and details

--- notebook/2020-11-13-functional-variants/funcvar.py
+++ notebook/2020-11-13-functional-variants/funcvar.py
@@ -4,18 +4,21 @@ from bsmcalls import SNPnexus
 
 clozukpath = '/home/attila/projects/bsm/resources/CLOZUK/supp-table-4.csv'
 
-def filtered_Dx_count(annot, filt='coding nonsyn', gb='Dx'):
+def filtered_Dx_count(annot, filt='coding nonsyn', gb='Dx', anygwas=None):
     data = annot.copy()
     data[gb] = pd.Categorical(data[gb])
     data[gb] = data[gb].cat.set_categories(list(data[gb].cat.categories) + ['Total'])
-    g = data.loc[data[filt].astype('bool'), [gb]].groupby(gb)
+    filt_pass = data[filt].astype('bool')
+    if anygwas is not None:
+        filt_pass = filt_pass & anygwas
+    g = data.loc[filt_pass, [gb]].groupby(gb)
     s = g.size()
     s['Total'] = s.sum()
     df = pd.DataFrame(s, columns=[filt]).T
     return(df)
 
-def filtered_Dx_counts(annot, filtl, gb='Dx'):
-    dfl = [filtered_Dx_count(annot, y, gb='Dx') for y in filtl]
+def filtered_Dx_counts(annot, filtl, gb='Dx', anygwas=None):
+    dfl = [filtered_Dx_count(annot, y, gb='Dx', anygwas=anygwas) for y in filtl]
     df = pd.concat(dfl, axis=0)
     return(df)
 
