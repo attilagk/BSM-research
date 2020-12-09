1dbb7c7963efdaeaab58968937366df128653bf8
Attila Jones <attila.g.jones@gmail.com>
2020-12-09 10:16:06 -0500

    readVCF.py now annotates also the Walsh data

--- src/bsmcalls/readVCF.py
+++ src/bsmcalls/readVCF.py
@@ -61,19 +61,17 @@ def sample_fromVCF(vcfpath):
 
 def convert_sample(bsm_sample):
     cmc_pattern = '^((MSSM|PITT)_([0-9]+))_(.*)$'
-    walsh_pattern = '^(([A-Z]+[^_]*))_((FrontalCortex))$'
+    walsh_pattern = '^([A-Z]+[^_]*)$'
     if re.match(cmc_pattern, bsm_sample):
-        pattern = cmc_pattern
-        prefix = 'CMC_'
+        s = re.sub(cmc_pattern, '\\1:\\4', bsm_sample)
+        l = s.split(':')
+        indiv_id = 'CMC_' + l[0]
+        tissue = l[1]
     elif re.match(walsh_pattern, bsm_sample):
-        pattern = walsh_pattern
-        prefix = ''
+        indiv_id = bsm_sample
+        tissue = 'frontal cortex'
     else:
         raise ValueError('Unexpected sample name: ' + bsm_sample)
-    s = re.sub(pattern, '\\1:\\4', bsm_sample)
-    l = s.split(':')
-    indiv_id = prefix + l[0]
-    tissue = l[1]
     return((indiv_id, tissue))
 
 def readVCF(vcfpath, annotlist=read_annotlist()):
