64315003b1c4bb9220646587818bab2b97a83b8f
Attila Jones <attila.g.jones@gmail.com>
2021-01-21 14:29:58 -0500

    Documentation for query and multiquery; fixed a bug in series_of_sets_intersect that occurred with all NaN inputs at fillna(0)

--- src/bsmcalls/operations.py
+++ src/bsmcalls/operations.py
@@ -5,11 +5,14 @@ import functools
 _level_names = ['Feature', 'Query']
 
 def series_of_sets_intersect(series_of_sets, query):
+    '''
+    Intersect query set with a pandas.Series of sets
+    '''
     if not isinstance(query, set):
         query = {query}
     s = series_of_sets.dropna()
-    s = s.apply(lambda x: len(x.intersection(query)))
-    s = s.reindex_like(series_of_sets).fillna(0).astype(np.int8)
+    s = s.apply(lambda x: len(x.intersection(query))).astype(np.int8)
+    s = s.reindex_like(series_of_sets).fillna(0, downcast=None).astype(np.int8)
     return(s)
 
 def dicts2sets(series_of_dicts, listvalued=True):
@@ -32,7 +35,10 @@ def scalar2sets(series_of_scalars):
     s = s.reindex_like(series_of_scalars)
     return(s)
 
-features = {'near_gens_Overlapped Gene': lambda x: x,
+features = {'ensembl_Gene': lambda x: x,
+            'ensembl_Symbol': lambda x: dicts2sets(x, False),
+            'ensembl_Predicted Function': lambda x: x,
+            'near_gens_Overlapped Gene': lambda x: x,
             'near_gens_Type': lambda x: dicts2sets(x, True),
             'near_gens_Annotation': lambda x: dicts2sets(x, True),
             'tfbs_TFBS Name': lambda x: x,
@@ -48,6 +54,71 @@ def anyquery(feature, data):
     return(df)
 
 def query(queryitems, feature, data):
+    '''
+    Query a feature in a dataframe
+
+    Arguments
+    queryitems: one or more strings to search for; see Examples 
+    feature: a column name in data
+    data: a DataFrame with rows: calls and columns: features
+
+    Value: a DataFrame of one count per queryitem and call
+
+    Details:
+    Depending on the type and content of queryitems the output DataFrame has
+    one or more columns with various semantics.  See Examples below
+
+    Examples:
+
+    I. Queryitems in a list
+
+    > query(['coding nonsyn', 'missense'], feature='near_gens_Annotation', data=annot)
+
+    Feature                                near_gens_Annotation
+    Query                                         coding nonsyn missense
+    Individual ID CHROM POS       Mutation
+    CMC_MSSM_027  1     11973569  C/T                         0        0
+                        67324428  A/T                         0        0
+                        182008461 C/T                         0        0
+                        207598957 C/T                         0        0
+                        219342012 C/T                         0        0
+    ...                                                     ...      ...
+
+    II. Queryitems in a set valued dict
+
+    > operations.query({'SCZ GWAS genes': gwasgenes}, 'near_gens_Overlapped Gene', data)
+
+    Feature                                near_gens_Overlapped Gene
+    Query                                             SCZ GWAS genes
+    Individual ID CHROM POS       Mutation
+    CMC_MSSM_027  1     11973569  C/T                              0
+                        67324428  A/T                              0
+                        182008461 C/T                              0
+                        207598957 C/T                              0
+                        219342012 C/T                              0
+    ...                                                          ...
+
+    III. When we search for any non NaN value in feature
+
+    > operations.query(None, 'phast_Score', data)
+
+    Feature                                phast_Score
+    Query                                          any
+    Individual ID CHROM POS       Mutation
+    CMC_MSSM_027  1     11973569  C/T            False
+                        67324428  A/T            False
+                        182008461 C/T            False
+                        207598957 C/T            False
+                        219342012 C/T            False
+    ...                                            ...
+    UMB914        8     137927145 C/T            False
+                  9     120507425 C/T            False
+                        137710239 C/G            False
+                  X     99255811  G/T            False
+                        130133640 C/T             True
+
+    [6426 rows x 1 columns]
+    '''
     names = _level_names
     if queryitems is None:
         return(anyquery(feature, data))
@@ -67,7 +138,40 @@ def query(queryitems, feature, data):
     df = pd.DataFrame(df.to_numpy(), index=df.index, columns=ix)
     return(df)
 
+
 def multiquery(querydict, data, do_sum=False, do_sort=False, margin=True):
+    '''
+    Query multiple features at once
+
+    Arguments
+    querydict: a dict where keys are features and values are lists or dicts (see Example)
+    data: an annot DataFrame
+    do_sum: whether to summarize results by calling summarize_query_results
+    do_sort: unsorted pandas multiindex gives a warning about low performance
+    margin: whether to calculate marginal counts 'All' in the summary
+
+    Value:
+    a DataFrame of counts either for each call if do_sum=False or aggregated
+    across all calls if do_sum=True
+
+    Example:
+
+    > querydict = {'sift_Prediction': ['Deleterious', 'Deleterious - Low Confidence'],
+                   'tfbs_TFBS Name': None,
+                   'near_gens_Overlapped Gene': {'SCZ GWAS genes': gwasgenes},
+                   'ensembl_Gene': {'brain enriched HPA': set(pa_enriched.index), 'brain elevated HPA': set(pa_elevated.index)},
+                   }
+    > multiquery(querydict, data, do_sum=True, do_sort=False, margin=True)
+
+    Dx                                                      Control  SCZ  ASD  All
+    Feature                   Query
+    sift_Prediction           Deleterious                         8    9    6   23
+                              Deleterious - Low Confidence        1    1    2    4
+    tfbs_TFBS Name            any                                48   78   50  176
+    near_gens_Overlapped Gene SCZ GWAS genes                     20   66   42  128
+    ensembl_Gene              brain enriched HPA                 42   69   59  170
+                              brain elevated HPA                211  344  280  835
+    '''
     l = [query(v, k, data) for k, v in querydict.items()]
     df = pd.concat(l, axis=1)
     if do_sort:
@@ -84,4 +188,6 @@ def summarize_query_results(results, data, margin=True):
         ix = pd.CategoricalIndex(results.columns, categories=categories)
         results = results.reindex(columns=ix)
         results['All'] = results.sum(axis=1)
+    results = results.astype('int64')
     return(results)
+
