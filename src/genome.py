29f4a2d7264c4e62af4d8643224ba5ca64921415
Attila Jones <attila.g.jones@gmail.com>
2021-04-06 10:15:16 -0400

    SCZ GWAS loci analyzed individually

--- src/genome.py
+++ src/genome.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import subprocess
 import io
+from bsmcalls import operations
 
 hs37d5_bed = '/big/data/refgenome/GRCh37/dna/hs37d5.bed'
+
+def read_scz_gwas():
+    fpath = '/home/attila/projects/bsm/resources/CLOZUK/supp-table-4.csv'
+    gwas = pd.read_csv(fpath, skiprows=7)
+    gwas['Chromosome'] = pd.Categorical(gwas['Chromosome'], ordered=True, categories=[str(y) for y in range(1, 23)] + list('XY'))
+    gwas.sort_values(['Chromosome', 'Start (BP)'], inplace=True)
+    return(gwas)
+
 
 def order_coordinates(df, coord_cols=['Chromosome', 'Start (BP)', 'End (BP)']):
     '''
@@ -43,3 +52,24 @@ def complement_intervals(df, coord_cols=['Chromosome', 'Start (BP)', 'End (BP)']
     val = pd.concat([df, cdf])
     val = order_coordinates(val, coord_cols)
     return(val)
+
+def annotate_with_gwas_loci(data, gwas=read_scz_gwas(), modify_return_data=True):
+    CHROM = data.index.get_level_values('CHROM')
+    POS = data.index.get_level_values('POS')
+    df = pd.DataFrame({'CHROM': CHROM, 'POS': POS}, index=data.index)
+    selcols = ['Chromosome', 'Start (BP)', 'End (BP)', 'P-value', 'Length (KB)']
+    for locus in gwas['Locus']:
+        chrom, start, end, pval, length = gwas.loc[gwas['Locus'] == locus, selcols].to_numpy()[0]
+        b = (df['CHROM'] == chrom) & (start <= df['POS']) & (df['POS'] <= end)
+        df.loc[b, 'SCZ GWAS locus'] = locus 
+        df.loc[b, 'SCZ GWAS p-value'] = pval 
+        df.loc[b, 'SCZ GWAS length (KB)'] = length 
+    df['SCZ GWAS locus'] = df['SCZ GWAS locus'].astype('Int64')#.astype('str')
+    coding = ~ data['near_gens_Overlapped Gene'].isna()
+    s = df.loc[coding, 'SCZ GWAS locus'].dropna()
+    df['SCZ GWAS locus, coding'] = s.reindex(data.index)
+    df.loc[coding, 'SCZ GWAS p-value, coding'] = df.loc[coding, 'SCZ GWAS p-value']
+    if modify_return_data:
+        newdata = pd.concat([data, df], axis=1)
+        return(newdata)
+    return(df)
