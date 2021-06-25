4cc6cedfc91e38b33767715320e258173414cf9d
Attila Jones <attila.g.jones@gmail.com>
2021-06-25 12:25:39 -0400

    Fixed regex for pythonpath in get_bsmdir

--- src/bsmutils.py
+++ src/bsmutils.py
@@ -4,5 +4,5 @@ import os.path
 
 def get_bsmdir():
     pythonpath = os.environ['PYTHONPATH']
-    bsmdir = re.sub('(^[^:]+/bsm)/src:.*$', '\\1', pythonpath)
+    bsmdir = re.sub('^(.+:)*([^:]+/bsm)/src:.*$', '\\2', pythonpath)
     return(bsmdir)
